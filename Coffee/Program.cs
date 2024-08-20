6c557441b9d40008bfbb945c26ea2736c834f309
Zeynep Tuana Çakır <2320711004@ogrenci.ibu.edu.tr>
2024-08-20 17:39:41 +0300

    Add project files.

---
+++ Coffee/Program.cs
@@ -0,0 +1,23 @@
+﻿
+List<string> coffee = new List<string>();
+
+Console.WriteLine("---- Favori Kahve Listenizi Oluşturun ---- ");
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("\nKahve " + (i + 1) + ": ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    coffee.Add(Console.ReadLine());
+    Console.ResetColor();
+
+}
+
+
+Console.WriteLine("\n\n---- Favori Kahve Listeniz ---- ");
+
+for (int i = 0; i < coffee.Count; i++)
+{
+    Console.WriteLine((i + 1) + "- " + coffee[i]);
+}
